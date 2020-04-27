5a891c3aa0e16bc897228aef59ab22f2db1948dd
WaschbaerDE <44826829+WaschbaerDE@users.noreply.github.com>
2020-04-27 19:55:57 +0200

    Add files via upload

---
+++ BarcodeParserGS1/BarcodeParserGS1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BarcodeParserGS1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Eingabe";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Analysieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(65, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Ergebnis";
+            
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Art";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 128);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Barcode Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BarcodeParserGS1/BarcodeParserGS1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BarcodeParserGS1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button1_click(object sender, EventArgs e)
+        {
+
+            //is Numeric schleife 1 (Mitarbeiter und NVE)
+            bool m = false;
+            int n = 2;
+            while (n < textBox1.Text.Length)
+            {
+                m = char.IsDigit(char.Parse(textBox1.Text.Substring(n, 1)));
+                //Console.WriteLine(m);
+                if (m is false)
+                { break; }
+                n++;
+            }
+
+            //is Numeric schleife 2 (Lagerplatz)
+            bool o = false;
+            int p = 7;
+            while (p < textBox1.Text.Length)
+            {
+                o = char.IsDigit(char.Parse(textBox1.Text.Substring(p, 1)));
+                //Console.WriteLine(o);
+                if (o is false)
+                { break; }
+                p++;
+            }
+
+            // Keine Eingabe aber Knopfgedrückt
+            if (textBox1.Text == "")
+            {
+                textBox2.Text = "Bitten geben Sie einen Code ein!";
+                textBox3.Text = "";
+                Console.WriteLine("Eingabe leer");
+            }
+
+            ///Mitarbeiter
+            else if (textBox1.Text.StartsWith("MA") & textBox1.Text.Length > 2 & m is true
+                 // ÜBERPRÜFEN AUF ZIFFERN& textBox1.Text.Remove(0,2) is int
+                 )
+            {
+                textBox2.Text = textBox1.Text.Remove(0, 2);
+                textBox3.Text = "Mitarbeiter";
+                Console.WriteLine("Mitarbeiter");
+            }
+
+            ///NVE
+            else if (
+                textBox1.Text.StartsWith("00") & textBox1.Text.Length is 20 & m is true
+                )
+            {
+                int a;
+                int b = Int32.Parse(textBox1.Text.Remove(0, 19));
+                a = (textBox1.Text.ElementAt(2) * 3 + textBox1.Text.ElementAt(3) + textBox1.Text.ElementAt(4) * 3 +
+                       textBox1.Text.ElementAt(5) + textBox1.Text.ElementAt(6) * 3 + textBox1.Text.ElementAt(7) + textBox1.Text.ElementAt(8) * 3 +
+                       textBox1.Text.ElementAt(9) + textBox1.Text.ElementAt(10) * 3 + textBox1.Text.ElementAt(11) + textBox1.Text.ElementAt(12) * 3 +
+                       textBox1.Text.ElementAt(13) + textBox1.Text.ElementAt(14) * 3 + textBox1.Text.ElementAt(15) + textBox1.Text.ElementAt(16) * 3 +
+                       textBox1.Text.ElementAt(17) + textBox1.Text.ElementAt(18) * 3) % 10 * -1 + 10;
+
+                ///Wenn der Modulo der Summe 0 ist und die letzte ziffer gleich 0 dann
+                if (
+                   (textBox1.Text.ElementAt(2) * 3 + textBox1.Text.ElementAt(3) + textBox1.Text.ElementAt(4) * 3 + textBox1.Text.ElementAt(5) +
+                   textBox1.Text.ElementAt(6) * 3 + textBox1.Text.ElementAt(7) + textBox1.Text.ElementAt(8) * 3 + textBox1.Text.ElementAt(9) +
+                   textBox1.Text.ElementAt(10) * 3 + textBox1.Text.ElementAt(11) + textBox1.Text.ElementAt(12) * 3 + textBox1.Text.ElementAt(13) +
+                   textBox1.Text.ElementAt(14) * 3 + textBox1.Text.ElementAt(15) + textBox1.Text.ElementAt(16) * 3 + textBox1.Text.ElementAt(17) +
+                   textBox1.Text.ElementAt(18) * 3) % 10 == 0 & textBox1.Text.Remove(0, 19) == "0"
+                   )
+                {
+                    textBox3.Text = "NVE";
+                    textBox2.Text = textBox1.Text.Remove(0, 2);
+                }
+                else if (a - b == 0)
+                {
+                    textBox3.Text = "NVE";
+                    textBox2.Text = textBox1.Text.Remove(0, 2);
+                }
+                else
+                {
+                    textBox2.Text = "Die eingegebene NVE ist nicht valide!";
+                    textBox3.Text = "Überprüfen Sie Ihre Eingabe!";
+                };
+            }
+
+            ///Lagerplatz
+            else if (
+                textBox1.Text.StartsWith("#LP#") & textBox1.Text.Length == 22 & o is true
+                )
+            {
+                textBox3.Text = "Lagerplatz";
+                textBox2.Text = textBox1.Text.Substring(4, 3) + "/" + textBox1.Text.Substring(7, 3) + "/" +
+                    textBox1.Text.Substring(10, 3) + "/" + textBox1.Text.Substring(13, 3) + "/" + textBox1.Text.Substring(16, 3) +
+                    "/" + textBox1.Text.Substring(19, 3);
+
+                Console.WriteLine(("Lagerplatz: " + textBox1.Text.Substring(4, 3) + "/" + textBox1.Text.Substring(7, 3) + "/" +
+                    textBox1.Text.Substring(10, 3) + "/" + textBox1.Text.Substring(13, 3) + "/" + textBox1.Text.Substring(16, 3) +
+                    "/" + textBox1.Text.Substring(19, 3)));
+            }
+
+            ///Kommission
+            else if (
+                textBox1.Text.StartsWith("KO") & textBox1.Text.Contains(",KU")
+                )
+            {
+                textBox3.Text = "Kommission";
+                textBox2.Text = textBox1.Text.Remove(0, (textBox1.Text.IndexOf(",") + 3)) + " - " + textBox1.Text.Remove(0, 2).Remove(textBox1.Text.IndexOf(",") - 2, (textBox1.Text.Length) - textBox1.Text.IndexOf(","));
+
+                Console.WriteLine(("Kommission: ") + textBox1.Text.Remove(0, (textBox1.Text.IndexOf(",") + 3)) + " - " + textBox1.Text.Remove(0, 2).Remove(textBox1.Text.IndexOf(",") - 2, (textBox1.Text.Length) - textBox1.Text.IndexOf(",")));
+            }
+
+            ///Kein Valider Code
+            else
+            {
+                textBox2.Text = "Der eingegebene Code ist invalide!";
+                textBox3.Text = "Überprüfen Sie Ihre Eingabe!";
+                Console.WriteLine("Kein valider Code");
+            }
+        }
+    }
+}
+
